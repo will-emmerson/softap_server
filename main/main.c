42554e928cd678f4b862b42110342ccaa59e98cf
willemmerson <willemmerson@gmail.com>
2020-11-11 18:22:14 +0000

    get running

--- main/main.c
+++ main/main.c
@@ -2,17 +2,19 @@
 #include <esp_log.h>
 #include <esp_netif.h>
 #include <limits.h>
-#include <network.h>
 #include <webserver.h>
 #include <wifi.h>
+#include <nvs_flash.h>
 
 static const char *TAG = "MAIN";
 
 __unused void app_main() {
 //    esp_log_level_set("*", ESP_LOG_INFO);
 //    network_init();
-    esp_netif_init();
+
+    ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     start_webserver();
+    ESP_ERROR_CHECK(nvs_flash_init());
     wifi_init();
 }
--- main/wifi.c
+++ main/wifi.c
@@ -39,14 +39,14 @@ static const char *TAG = "WIFI";
 
 
 void wifi_init() {
+    esp_netif_create_default_wifi_ap();
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 //    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
-    esp_netif_create_default_wifi_ap();
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
     wifi_config_t ap_config = {.ap = {.authmode = WIFI_AUTH_OPEN, .max_connection=5,}};
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &ap_config));
-    esp_wifi_connect();
+    esp_wifi_start();
 }
 
 
