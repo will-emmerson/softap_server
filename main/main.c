1d08ab2d9969f32bf89fe69800571ad5295ad586
willemmerson <willemmerson@gmail.com>
2020-11-12 09:09:41 +0000

    more cleanup

--- main/main.c
+++ main/main.c
@@ -9,9 +9,6 @@
 static const char *TAG = "MAIN";
 
 __unused void app_main() {
-//    esp_log_level_set("*", ESP_LOG_INFO);
-//    network_init();
-
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     start_webserver();
